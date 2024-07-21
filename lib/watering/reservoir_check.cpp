1e1eda200c0cf6c68ca3b84482fb1e99dd9f2c74
Chris Lee <lee.chris.h@gmail.com>
2024-07-21 16:59:49 -0400

    copyrights

--- lib/watering/reservoir_check.cpp
+++ lib/watering/reservoir_check.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2024 Chris Lee and contibuters.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
 #include "reservoir_check.h"
 
 #include "og3/ha_discovery.h"
